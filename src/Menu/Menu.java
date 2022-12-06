4079ba0aaf590fb8db4f96ad177fb6fa38bd257a
VITOR SANTANA DE JESUS <santanav92@gmail.com>
2022-12-06 17:49:34 -0300

    Treino-Java

---
+++ src/Menu/Menu.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Menu;
+
+import Conta.Conta;
+import java.util.Scanner;
+
+public class Menu {
+
+    public void executaMenu() {
+        System.out.println("###### MENU ######");
+        System.out.println("ESCOLHA UMA OPÇÃO: ");
+        System.out.println("1  - SALDO");
+        System.out.println("2  - DEPÓSITO");
+        System.out.println("3  - SAQUE");
+
+        Scanner objScanner = new Scanner(System.in);
+        Conta objConta = new Conta();
+
+        int operacao = objScanner.nextInt();
+        float valor = 0;
+
+        switch (operacao) {
+
+            case 1:
+                objConta.consultaSaldo();
+                break;
+
+            case 2:
+                System.out.println("Qual o valor do depósito que deseja fazer: ");
+                valor = objScanner.nextFloat();
+                objConta.geraDeposito(valor);
+                objConta.consultaSaldo();
+                break;
+
+            case 3:
+                System.out.println("Qual o valor do saque que deseja fazer: ");
+                valor = objScanner.nextFloat();
+                objConta.geraSaque(valor);
+                objConta.consultaSaldo();
+                break;
+                
+            default: 
+                System.out.println("O Valor digitado não atende aos requisitos propostos");       
+        }
+    }
+
+}
---
+++ src/Principal/Principal.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Principal;
+
+import Menu.Menu;
+
+
+/**
+ *
+ * @author ❤🚀❤ Vitor de Jesus ❤🚀❤
+ */
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    
+        Menu objMenu = new Menu();
+        
+        objMenu.executaMenu();  
+        
+        
+    }
+    
+}
