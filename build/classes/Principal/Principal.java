b3fc1e10de6fbb42c8924d425c1a6ef3328f2325
Vitor de Jesus <>
2022-12-07 16:35:42 -0300

    "Treino-Java"

---
+++ build/classes/Principal/Principal.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Principal;
+
+import MenuPrincipal.MenuPrincipal;
+
+
+/**
+ *
+ * @author ❤🚀❤ Vitor de Jesus ❤🚀❤
+ */
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    
+        MenuPrincipal  objMenuPrincipal = new MenuPrincipal();
+        
+        objMenuPrincipal.executaMenuPrincipal();  
+        
+        
+    }
+    
+}
